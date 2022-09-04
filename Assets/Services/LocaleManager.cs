3b243281d32ee61e8701de30581cf9054c4f1e3c
dim <web.best.master@gmail.com>
2022-09-04 22:20:54 +0200

    upd

--- Assets/Services/LocaleManager.cs
+++ Assets/Services/LocaleManager.cs
@@ -1,13 +1,27 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
 
 public class LocaleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        StartCoroutine(UpdateLocale());
+    }
+
+    private IEnumerator UpdateLocale()
+    {
+
+        while (LocalizationSettings.AvailableLocales.Locales.Count < 2)
+        {
+            Debug.Log("[LocaleManager] wait for LocalizationSettings.AvailableLocales.Locales is defined");
+            // Debug.Log(LocalizationSettings.AvailableLocales.Locales.Count);
+            yield return new WaitForSeconds(0.1f);            
+        }
+
+        Debug.Log("[LocaleManager] set locale");
+        // Debug.Log(LocalizationSettings.AvailableLocales.Locales.Count);
+
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
     }
 }
